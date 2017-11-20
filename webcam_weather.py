2e6067fbe6160398a0e39fcc5474c3e58dcf4014
Aaron C <ajc21@sfu.ca>
2017-11-20 15:45:57 -0800

    Read CSVs from dir into dataframe and read images from dir

--- webcam_weather.py
+++ webcam_weather.py
@@ -1,8 +1,19 @@
 import pandas as pd
 import numpy as np
+import sys
+from skimage.io import imread_collection
+import glob
+import os
 
 def main(csv_directory, img_directory):
-    weather = pd.read_csv(csv_directory, parse_dates=['Date/Time'], header=14)
+    
+    allFiles = glob.glob(csv_directory + "/*.csv")
+    df = pd.concat((pd.read_csv(f, parse_dates=['Date/Time'], header=14) for f in allFiles), ignore_index=True)
+    # TODO: clean weather data
+    
+    #print(df)
+    imgs = imread_collection(os.path.join(img_directory,"*.jpg"))
+    print(imgs) #imgs[0] etc
     
 
 if __name__=='__main__':
