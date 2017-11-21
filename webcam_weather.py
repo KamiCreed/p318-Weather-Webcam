fffac37628010ece8dccdde6261a8f51b2424569
Sami Ma <masamim@sfu.ca>
2017-11-20 17:00:57 -0800

    Join image names and csv on datetime

--- webcam_weather.py
+++ webcam_weather.py
@@ -2,19 +2,35 @@ import pandas as pd
 import numpy as np
 import sys
 from skimage.io import imread_collection
+from skimage.io import ImageCollection
 import glob
 import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 
 def main(csv_directory, img_directory):
     
     allFiles = glob.glob(csv_directory + "/*.csv")
     df = pd.concat((pd.read_csv(f, parse_dates=['Date/Time'], header=14) for f in allFiles), ignore_index=True)
-    # TODO: clean weather data
     
-    #print(df)
-    imgs = imread_collection(os.path.join(img_directory,"*.jpg"))
-    print(imgs) #imgs[0] etc
+    df = df.dropna(subset=['Weather'])
+#    print(df)
     
+#    imgs = imread_collection(os.path.join(img_directory,"*.jpg"))
+    imgs = ImageCollection(os.path.join(img_directory,"*.jpg"))
+    labels = pd.DataFrame([os.path.basename(f) for f in imgs.files], columns=['filename'])
+    labels['string'] = labels.filename.str.extract('(\d+)', expand=True).astype(str)
+    labels['Date/Time'] = pd.to_datetime(labels.string)
+#    print(labels)
+    
+    joined = labels.set_index('Date/Time').join(df.set_index('Date/Time'))
+#    print(joined)
+    
+    X_train, X_test, y_train, y_test = train_test_split(imgs, joined.Weather.values)
+    model = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(4, 3),
+                      activation='logistic')
+    model.fit(X_train, y_train)
 
 if __name__=='__main__':
     csv_directory = sys.argv[1]
