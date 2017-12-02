f1358f4ef04eaaddd6454e61e4b52ae76e5c4e1f
Aaron C <ajc21@sfu.ca>
2017-12-01 17:30:10 -0800

    Add time of day prediction, condense img loading code, reduce PCA to 50

--- webcam_weather.py
+++ webcam_weather.py
@@ -8,7 +8,9 @@ import os
 import gc
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
 from PIL import Image
+from sklearn.ensemble import BaggingClassifier
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -43,24 +45,36 @@ def np_clean_labels(weather_labels):
     return all_labels
     
 def load_imgs(X_path):
-    l = []
-    for img_path in X_path:
-        img2d = io.imread(img_path)
-        l.append(img2d.ravel())
-    return np.asarray(l)
+    # From https://stackoverflow.com/questions/27841554/array-of-images-in-python
+    arr = np.array([np.array(io.imread(img).ravel()) for img in X_path])
+    return arr
 
 def load_imgs_processing(X_path):
-    l = []
-    for img_path in X_path:
-        img2d = io.imread(img_path)
-#        misc.imshow(img2d)
-#        input("Press enter for processing...")
-#        img2d = exposure.equalize_hist(img2d)
-#        misc.imshow(img2d)
-#        input("Press enter to continue...")
-        img2d = filters.gaussian(img2d, sigma=8, multichannel=True)
-        l.append(img2d.ravel())
-    return np.asarray(l)
+    # From https://stackoverflow.com/questions/27841554/array-of-images-in-python
+    arr = np.array([np.array(io.imread(img).ravel()) for img in X_path])
+    # arr = np.empty([length,1])
+#     for i in range(0,length):
+#         img2d = io.imread(X_path[i])
+# #        misc.imshow(img2d)
+# #        input("Press enter for processing...")
+# #        img2d = exposure.equalize_hist(img2d)
+# #        misc.imshow(img2d)
+# #        input("Press enter to continue...")
+#         #img2d = filters.gaussian(img2d, sigma=8, multichannel=True)
+#         arr[i] = img2d.ravel()
+    return arr
+
+# Note Katkam images are only taken during daylight hours (usually 6-18 or 6AM to 6PM)
+def hour_to_timeofday(hour_str):
+    hour = int(hour_str[0:2])
+    if hour > 6:
+        return 'Early morning'
+    elif hour >= 6 and hour <= 12:
+        return 'Morning'
+    elif hour > 12 and hour < 18:
+        return 'Afternoon'
+    elif hour >= 19 and hour <= 24:
+        return 'Evening'
 
 def main():
     if len(sys.argv) < 4:
@@ -94,16 +108,26 @@ def main():
     labels = pd.DataFrame(names, columns=['filename'])
     labels['paths'] = pd.Series(paths)
     labels['string'] = labels.filename.str.extract('(\d+)', expand=True).astype(str)
-    labels['Date/Time'] = pd.to_datetime(labels.string)
+    labels['DateTime'] = pd.to_datetime(labels.string)
+    joined = labels.set_index('DateTime').join(df.set_index('Date/Time'))
     
-    joined = labels.set_index('Date/Time').join(df.set_index('Date/Time'))
-    
+    # Target == weather
     if column_id == 0:
         joined = joined.dropna(subset=['Weather'])
         y = np_clean_labels(joined['Weather']).values
         y = MultiLabelBinarizer().fit_transform(y)
+    # Target == Time of day
     elif column_id == 1:
-        y = joined.index.hour
+        #y = joined.index.hour
+        #joined['timeofday'] = pd.to_datetime(joined.Time)
+        #hour_to_timeofday('21:00:00')
+        
+        joined['time_of_day'] = joined['Time'].apply(hour_to_timeofday)
+        #print(joined)
+        y = joined['time_of_day'].values
+    # Target == Visibility
+    elif column_id == 2:
+        return
     else:
         print("No training of column.")
         return
@@ -120,13 +144,19 @@ def main():
     # got 0.00542 with MLP, OneVsRest, MultiLabelBinarizer
     #X_train, X_test, y_train, y_test = train_test_split(X, joined['clean'].values) #doesn't work because multilabel
 
+    n_estimators = 10
     model = make_pipeline(
-                PCA(40000),
-#                MLPClassifier(solver='lbfgs', hidden_layer_sizes=(100),
-#                      activation='logistic')
-                KNeighborsClassifier(n_neighbors=20)
+                PCA(50),
+                #OneVsRestClassifier(MLPClassifier(solver='adam', hidden_layer_sizes=(50),
+                #     activation='logistic'))
+                #SVC(C=1) # doesn't support multilabel
+                KNeighborsClassifier(n_neighbors=13)
                 )
-    
+    # 0.49 with PCA 50, MLP adam hidden layers=50 logistic
+    # 0.407859078591 with One Vs Rest MLP adam hiddenlayers=50 logistic
+    # 0.592140921409 with KNeighbors, n_neighbors=20, PCA 50
+    # 0.605691056911 with KNeighbors, n_neighbors=15, PCA 50
+    # 0.634146341463 n_neighbors=13
     model.fit(X_train, y_train)
     
     X_test = load_imgs(X_test_paths)
